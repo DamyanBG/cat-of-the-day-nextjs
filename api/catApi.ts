b0133123b84ee8be608233b6939459d8e8295f67
Damyan Dimitrov <d.dimitrov.1988@abv.bg>
2024-01-01 19:24:41 +0200

    fix axios

--- api/catApi.ts
+++ api/catApi.ts
@@ -5,7 +5,13 @@ import DefaultCatPhoto from "@/public/images/default-cat-of-the-week.jpg"
 import { GetCatOfTheDayResponse } from "@/types/api/responses"
 
 export const getCatOfTheDay = async () => {
-    const response = await axios.get<GetCatOfTheDayResponse>(`${HOST_URL}/cat-of-the-day-photo`)
-    const photoUrl = response.data.message ? DefaultCatPhoto.src : response.data.cat_of_the_day
+    let photoUrl
+    try {
+        const response = await axios.get<GetCatOfTheDayResponse>(`${HOST_URL}/cat-of-the-day-photo`)
+        photoUrl = response.data.cat_of_the_day
+    } catch (error) {
+        console.log(error)
+        photoUrl = DefaultCatPhoto.src
+    }
     return photoUrl
 }

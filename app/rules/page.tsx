da5f0f7ee5b10742334b086f3bcb4e37704c1cc9
Damyan Dimitrov <d.dimitrov.1988@abv.bg>
2024-01-01 19:36:00 +0200

    style for rules

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,18 +1,18 @@
 export default function Rules() {
     return (
         <main>
-            <section>
+            <section className="rules">
                 <h2>Rules!!!</h2>
                 <p>
-                    The cat of the day is the cat that received the most likes
-                    excluding dislikes from the previous day!
+                    The cat of the week is the cat that received the most likes
+                    excluding dislikes from the previous week!
                 </p>
                 <p>
                     When you upload a cat, voting for it starts from the next
-                    day, ensuring that every cat has equal time for voting!
+                    week, ensuring that every cat has equal time for voting!
                 </p>
                 <p>
-                    Only registered participants can upload cats, and they must
+                    Only registered user can upload cats, and they must
                     accept the terms and conditions before being able to
                     participate in the game!
                 </p>
@@ -20,8 +20,9 @@ export default function Rules() {
                     Once a cat is uploaded, the information cannot be changed!
                 </p>
                 <p>
-                    The voting is reset every day, and a new round of voting
-                    begins!
+                    The voting is reset every week, and a new round of voting
+                    begins! All cats with their information and photos are deleted, 
+                    except the cat, which wins Cat of the week!
                 </p>
                 <p>
                     For now there is no prize for the winners, but in future we

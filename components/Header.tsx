be61028dc33e86ad6fbef2e4be162abd4880bedb
DamyanBG <d.dimitrov.1988@abv.bg>
2024-01-02 15:50:17 +0200

    change header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,37 @@
+'use client'
+
 import Link from "next/link";
 
 import AuthNav from "./AuthNav";
 import PageNav from "./PageNav";
+import { useState } from "react";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState<boolean>(false)
+  const navClassName = isActive ? "active" : ""
+
+  const handleHamburgerClick = () => {
+    setIsActive(!isActive)
+  }
+
+  const handleCloseMenu = () => {
+    setIsActive(false)
+  }
+
   return (
     <header>
       <article className="site-name">
-        <Link href="/">Cat of the week beta</Link>
+        <Link href="/">Cat of the week</Link>
       </article>
-      <PageNav />
-      <AuthNav />
+      <article className="hamburger" onClick={handleHamburgerClick}>
+          <article className="line" />
+          <article className="line" />
+          <article className="line" />
+      </article>
+      <section className={navClassName} onClick={handleCloseMenu}>
+        <PageNav />
+        <AuthNav />
+      </section>
     </header>
   );
 };

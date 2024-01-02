f21289efba9570e145e37c05bb7067ee2341cb6e
DamyanBG <d.dimitrov.1988@abv.bg>
2024-01-02 15:57:22 +0200

    home page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     return (
         <main>
             <section className="top-section">
-                <article className="home-image">
-                    <CatOfTheWeekComponent />
-                </article>
                 <article className="home-text">
                     <h1>Welcome to Cat of the week!</h1>
                     <br />
                     <p>A platform for real cat lovers!</p>
+                </article>
+                <article className="home-image">
+                    <CatOfTheWeekComponent />
                 </article>
             </section>
         </main>
